6e6e8e5918d780d2b3d7d6e747e67a8d8eda6f97
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-22 20:11:07 +0530

    Create controller function of forgot password

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -9,7 +9,7 @@ const cookieOptions = {
   secure: true,
 };
 
-// User registration controller
+// controller function to register user
 const register = async (req, res, next) => {
   try {
     // Extract information from request body
@@ -84,7 +84,7 @@ const register = async (req, res, next) => {
   }
 };
 
-// User login controller
+// controller function to login user
 const login = async (req, res, next) => {
   try {
     // Extract information
@@ -130,7 +130,7 @@ const login = async (req, res, next) => {
 };
 
 
-// User logout controller
+// controller function to user logout process
 const logout = (req, res) => {
   try {
     // Clear the JWT token in the cookie
@@ -167,4 +167,67 @@ const getProfile = async (req, res, next) => {
   }
 };
 
-export { register, login, logout, getProfile };
+// controller function to initiate forgot password process
+const forgotPassword = async (req, res, next) => {
+  try {
+    // Extract the email from the request body
+    const { email } = req.body;
+
+    // Check if the email is provided or not
+    if (!email) {
+      return next(new AppError(400, "Email is required"));
+    }
+
+    // Check if the email exists in the database
+    const emailExists = await User.findOne({ email });
+
+    if (!emailExists) {
+      return next(new AppError(400, "Email not registered"));
+    }
+
+    // Generate the forgot password token for the user
+    const forgotPasswordToken = await emailExists.generateForgotPasswordToken();
+
+    // Save the user with the updated forgot password token and expiry
+    await emailExists.save();
+
+    // Create the forgot password URL for the email
+    const forgotPasswordURL = `${process.env.CLIENT_URL}/forgot-password/${forgotPasswordToken}`;
+    const subject = "Reset Password";
+
+    // Create the content of the reset password email as an HTML link
+    const message = `
+      <h1>Dear ${emailExists.fullName},</h1>
+      <p>You have requested to reset your password. Please click on the following link to reset your password:</p>
+      <a href="${forgotPasswordURL}">${forgotPasswordURL}</a>
+      <p>If you did not request this password reset, you can ignore this email.</p>
+      <p>Best regards,</p>
+      <p>Shoaib Hasan</p>
+    `;
+
+    try {
+      // Send the reset password email to the user
+      await sendEmail(email, subject, message);
+
+      // Respond with success message
+      res.status(200).json({
+        success: true,
+        message: "Reset password email sent successfully",
+      });
+    } catch (error) {
+      // If an error occurs while sending the email, handle the error
+      // and reset the forgot password token and expiry for the user
+      emailExists.forgotPasswordToken = undefined;
+      emailExists.forgotPasswordExpiry = undefined;
+
+      await emailExists.save();
+
+      return next(new AppError(500, "Error sending reset password email"));
+    }
+  } catch (error) {
+    // If an error occurs in the try block, handle the error
+    return next(new AppError(500, error.message));
+  }
+};
+
+export { register, login, logout, getProfile, forgotPassword };
