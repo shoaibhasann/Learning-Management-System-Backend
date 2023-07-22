429a5cd306a685fd4b72bdc8a499c3851d54127d
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-22 16:50:53 +0530

    Add cloudinary configuration in server

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import dotenv from "dotenv"
 import app from "./app.js";
 import connectDatabase from "./config/db.js";
+import cloudinary from "cloudinary";
 
 // Load enviroment variables
 dotenv.config();
 
 const port = process.env.PORT || 8001;
+
+// cloudinary configuration 
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 // start the server
 app.listen(port, async() => {
