e54bb384979d74dc6f8c9cf35e4d060478e99197
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-22 17:17:58 +0530

    Add middleware in register route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { register, login, logout, getProfile} from "../controllers/user.controllers.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/multer.middleware.js";
+
 
 const router = Router();
 
 // user routes
-router.post('/register', register);
+router.post('/register', upload.single('avatar'), register);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/me', isLoggedIn, getProfile);
