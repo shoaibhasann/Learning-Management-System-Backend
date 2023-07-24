8c48a0b0f80602a06193bdfe628b48f23a3d1c5a
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-24 15:49:26 +0530

    Fix some issue

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -18,7 +18,7 @@ const isLoggedIn = async (req,res,next) => {
 const authorizedRoles = (...roles) => async (req,res,next) => {
 
     const currentUserRole = req.user.role;
-
+    
     if(!roles.includes(currentUserRole)){
         return next(
             new AppError(403, 'You do not have permission to acess this route.')
