b243f6d1d8dacb145f8e4cb0729ed5fb0803eec7
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-31 02:50:33 +0530

    Integrate razorpay gateway

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 import app from "./app.js";
 import connectDatabase from "./config/db.js";
 import cloudinary from "cloudinary";
+import Razorpay from "razorpay";
 
 // Load enviroment variables
 dotenv.config();
@@ -13,6 +14,12 @@ cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Razorpay gateway configuration
+export const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_SECRET
 });
 
 // start the server
